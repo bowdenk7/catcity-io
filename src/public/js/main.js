ac1024296d6e145533e87bb66f1e0abfc823d21e
Bowden <wilkelly@microsoft.com>
2018-02-01 17:25:32 -0800

    removed tinder stuff

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,31 +4,10 @@ $(document).ready(function() {
   for (const id of JSON.parse($("#catIds")[0].value)) {
     const selector = "#" + id;
     // @ts-ignore
-    $(selector).slick();
+    $(selector).slick({
+      centerMode: true,
+      centerPadding: false,
+      dots:true
+    });
   }
-
-  // @ts-ignore
-  stack = window.swing.Stack();
-
-  [].forEach.call(document.querySelectorAll(".stack li"), function (targetElement) {
-    let card = stack.createCard(targetElement);
-    card.on('dragmove', (e) => {
-      console.log(e);
-    })
-    targetElement.classList.add("in-deck");
-  });
-
-  stack.on("throwout", function (e) {
-    console.log(e.target.innerText || e.target.textContent, "has been thrown out of the stack to the", e.throwDirection, "direction.");
-
-    e.target.classList.remove("in-deck");
-  });
-
-  stack.on("throwin", function (e) {
-    console.log(e.target.innerText || e.target.textContent, "has been thrown into the stack from the", e.throwDirection, "direction.");
-
-    e.target.classList.add("in-deck");
-  });
-
-
 });
